c13250c3c27e46b7311b84e3d63b5957030498ea
BlackDeluxeCat <65377021+BlackDeluxeCat@users.noreply.github.com>
2023-07-14 18:13:52 +0800

    init

---
+++ src/de/MindustryDatabaseExt.java
@@ -0,0 +1,16 @@
+package de;
+
+import arc.*;
+import arc.util.*;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class MindustryDatabaseExt extends Mod{
+
+    public MindustryDatabaseExt(){
+        Events.on(ClientLoadEvent.class, e -> {
+
+        });
+    }
+}
